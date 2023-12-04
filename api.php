e38ee3124c1ac02e827ad07deb13352fb874089d
s4ngnm <141365785+s4ngnm@users.noreply.github.com>
2023-12-04 19:58:43 +0700

    1

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "sangnm";
 $dbname = "db_quanlyvia";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
