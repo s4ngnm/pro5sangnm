9ee298a85c693900fa4dde19dd94d5d199581e32
s4ngnm <141365785+s4ngnm@users.noreply.github.com>
2023-12-04 19:59:46 +0700

    1

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
             { 
                 data: null,
                 render: function(data, type, row) {
-                    return '<button onclick="editProxy(\'' + row.id + '\')">Edit</button>' +
-                           '<button onclick="deleteProxy(\'' + row.id + '\')">Delete</button>';
+                    return '<button class="btn btn-warning btn-sm" onclick="editProxy(\'' + row.id + '\')">Edit</button>' +
+                           '<button class="btn btn-danger btn-sm" onclick="deleteProxy(\'' + row.id + '\')">Delete</button>';
                 }
             }
         ]
@@ -22,43 +22,14 @@ $(document).ready(function() {
     function reloadTable() {
         table.ajax.reload();
     }
-// Function to add a new proxy using modal
-    window.addProxy = function() {
-        var fullFormat = prompt('Enter Proxy (IP:Port:Username:Password)');
-        var dateCreated = prompt('Enter Date Created');
-        var dateExpired = prompt('Enter Date Expired');
-        var note = prompt('Enter Note');
 
-        $.post('api.php', {
-            addProxy: true,
-            fullFormat: fullFormat,
-            dateCreated: dateCreated,
-            dateExpired: dateExpired,
-            note: note
-        }, function() {
-            reloadTable();
-        });
+    window.addProxy = function() {
         openModal();
     };
 
     window.editProxy = function(id) {
-        var fullFormat = prompt('Enter Proxy (IP:Port:Username:Password)');
-        var dateCreated = prompt('Enter Date Created');
-        var dateExpired = prompt('Enter Date Expired');
-        var note = prompt('Enter Note');
-
-        $.post('api.php', {
-            editProxy: true,
-            id: id,
-            fullFormat: fullFormat,
-            dateCreated: dateCreated,
-            dateExpired: dateExpired,
-            note: note
-        }, function() {
-            reloadTable();
-        });
         openModal();
-        // Fetch proxy details and pre-fill the form
+
         $.get('api.php?id=' + id, function(data) {
             if (data.length > 0) {
                 var proxy = data[0];
@@ -67,7 +38,29 @@ $(document).ready(function() {
                 document.getElementById('dateExpired').value = proxy.dateExpired;
                 document.getElementById('note').value = proxy.note;
             }
-            });
+        });
+    };
+
+    window.saveProxy = function() {
+        var id = '';
+        var fullFormat = document.getElementById('fullFormat').value;
+        var dateCreated = document.getElementById('dateCreated').value;
+        var dateExpired = document.getElementById('dateExpired').value;
+        var note = document.getElementById('note').value;
+
+        var action = id === '' ? 'addProxy' : 'editProxy';
+
+        $.post('api.php', {
+            [action]: true,
+            id: id,
+            fullFormat: fullFormat,
+            dateCreated: dateCreated,
+            dateExpired: dateExpired,
+            note: note
+        }, function() {
+            reloadTable();
+            closeModal();
+        });
     };
 
     window.deleteProxy = function(id) {
@@ -83,44 +76,11 @@ $(document).ready(function() {
         }
     };
 
-    // Function to save proxy from modal
-    window.saveProxy = function() {
-        var id = ''; // You may need to include the ID if it's an edit operation
-        var fullFormat = document.getElementById('fullFormat').value;
-        var dateCreated = document.getElementById('dateCreated').value;
-        var dateExpired = document.getElementById('dateExpired').value;
-        var note = document.getElementById('note').value;
-
-        // Determine if it's an add or edit operation
-        var action = id === '' ? 'addProxy' : 'editProxy';
-
-        // Send data to the server using AJAX
-        $.post('api.php', {
-            [action]: true,
-            id: id,
-            fullFormat: fullFormat,
-            dateCreated: dateCreated,
-            dateExpired: dateExpired,
-            note: note
-        }, function() {
-            reloadTable();
-            closeModal(); // Close the modal after saving
-        });
-    };
-
-    // ... (Phần JavaScript trước đó)
-
-    // Function to open modal for adding/editing proxy
     window.openModal = function() {
         document.getElementById('proxyModal').style.display = 'block';
     };
 
-    // Function to close modal
     window.closeModal = function() {
         document.getElementById('proxyModal').style.display = 'none';
     };
-
-// ... (Phần JavaScript tiếp theo)
-
-
 });
